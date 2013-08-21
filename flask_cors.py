91688c00d81e252869402c73767bc3fece834bc1
Mohit Soni <mohitsoni1989@gmail.com>
2013-08-20 23:51:33 -0700

    Updated extension to be a middleware

--- flask_cors.py
+++ flask_cors.py
@@ -3,27 +3,38 @@
     flaskext.cors
     ~~~~~~~~~~~~~
 
-    Adds CORS support to your application.
+    CORS middleware, adds CORS support to your application.
 
+    :author: Mohit Soni
     :license: MIT, see LICENSE for more details.
 """
 
 
-from flask import make_response, request, current_app
+class CORSMiddleware(object):
+    """Wrap the application in this middleware.
 
+    :param app: the WSGI application
+    :param app_root: Defaulting to ``'/'``, you can set this to something else
+        if your app is mounted somewhere else.
+    :param cors_config: the configuration
+    """
+    def __init__(self, app, app_root='/', cors_config=None):
+        self.app = app
+        self.app_root = app_root
+        self.cors_config = cors_config
 
-def cors(config=None):
-    def init_default_config(config):
-        config['CORS_ALLOWED_ORIGINS'] = config['CORS_ALLOWED_ORIGINS'] if 'CORS_ALLOWED_ORIGINS' in config and config['CORS_ALLOWED_ORIGINS'] is not None else '*'
-        config['CORS_ALLOWED_METHODS'] = config['CORS_ALLOWED_METHODS'] if 'CORS_ALLOWED_METHODS' in config and config['CORS_ALLOWED_METHODS'] is not None else ['GET','POST','HEAD','OPTIONS']
-        config['CORS_ALLOWED_HEADERS'] = config['CORS_ALLOWED_HEADERS'] if 'CORS_ALLOWED_HEADERS' in config and config['CORS_ALLOWED_HEADERS'] is not None else ['Origin','Accept','X-Requested-With','Content-Type','Access-Control-Request-Method','Access-Control-Request-Headers']
-        config['CORS_EXPOSED_HEADERS'] = config['CORS_EXPOSED_HEADERS'] if 'CORS_EXPOSED_HEADERS' in config and config['CORS_EXPOSED_HEADERS'] is not None else None
-        config['CORS_SUPPORT_CREDENTIALS'] = config['CORS_SUPPORT_CREDENTIALS'] if 'CORS_SUPPORT_CREDENTIALS' in config and config['CORS_SUPPORT_CREDENTIALS'] is not None else True
-        config['CORS_PREFLIGHT_MAXAGE'] = config['CORS_PREFLIGHT_MAXAGE'] if 'CORS_PREFLIGHT_MAXAGE' in config and config['CORS_PREFLIGHT_MAXAGE'] is not None else 1800
-        config['CORS_REQUEST_DECORATE'] = config['CORS_REQUEST_DECORATE'] if 'CORS_REQUEST_DECORATE' in config and config['CORS_REQUEST_DECORATE'] is not None else True
+    def __call__(self, environ, start_response):
+        # Check for the presence of 'Origin' header
+        if 'HTTP_ORIGIN' in environ.keys():
+            # Wrap it, to add CORS specific headers
+            def _cors_start_response(status, headers, *args):
+                """
+                Add CORS headers here
+                """
+                headers = []
 
-    
-    if config is None:
-        config = {}
-
-    init_default_config(config)
+                return start_response(status, headers, *args)
+            return self.app(environ, _cors_start_response)
+        else:
+            # Otherwise, let it be a pass through
+            return self.app(environ, start_response)
